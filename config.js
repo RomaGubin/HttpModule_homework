4e3fb99b9b7498ad624c8d7ddc06a6e6dadccf58
Roman G <roma.gubin2011@yandex.ru>
2024-05-12 11:16:17 +0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+  accessKey: process.env.accessKey || 'your_access_key_here'
+};
---
+++ index.js
@@ -0,0 +1,31 @@
+
+//запрашиваем погоду по городу или по координатам, пример: node index.js "Москва" или node index.js "55.753215,37.622504"
+
+const http = require('http');
+const config = require('./config');
+
+const accessKey = config.accessKey;
+
+const options = {
+    hostname: 'api.weather.yandex.ru',
+    path: '/v2/forecast?lat=52.37125&lon=4.89388',
+    headers: {
+        'X-Yandex-Weather-Key': accessKey
+    }
+};
+
+const req = http.get(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    res.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+});
+
+req.on('error', (error) => {
+    console.error(error);
+});
